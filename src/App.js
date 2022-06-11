005f74d6869ec55ebeb166ded6ab9c811efa00f3
Sesame <mushroom0505@gmail.com>
2022-06-11 11:07:30 +0800

    https://github.com/sesame55/3dicon.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
+// import { Link } from 'react-router-dom';
+// import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faTree, faPen, faWater, faHome } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
+    // 來源 https://www.youtube.com/watch?v=C8wSNZqktOM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <a href="/#">
+            {/* 不能用React的Link替代a，注意引入 */}
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+              <FontAwesomeIcon icon={faBook} />
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+              <FontAwesomeIcon icon={faTree} />
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+              <FontAwesomeIcon icon={faWater} />
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+              <FontAwesomeIcon icon={faPen} />
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+              <FontAwesomeIcon icon={faHome} />
+            </span>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
